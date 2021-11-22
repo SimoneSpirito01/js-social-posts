e8ecbe1f2278a94fada5b32dfac3b18e527809e0
SimoneSpirito <simospiri01@gmail.com>
2021-11-22 13:54:08 +0100

    BONUS - immagine profilo mancante

--- main.js
+++ main.js
@@ -55,3 +55,92 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.getElementById('container');
+let post = '';
+
+for (let i = 0; i < posts.length; i++){
+    if (posts[i].author.image != null){
+        post += 
+        `
+            <div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].content}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${posts[i].author.name}</div>
+                            <div class="post-meta__time">${posts[i].created}</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">${posts[i].content}</div>
+                <div class="post__image">
+                    <img src="${posts[i].media}" alt="${posts[i].author.name}-profile-image">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div>
+        `;
+    } else {
+        console.log(posts[i].author.name)
+        let init = inCharacter(posts[i].author.name)
+        post += 
+        `
+            <div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <div class="bcg-img profile-pic">${init}</div>
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${posts[i].author.name}</div>
+                            <div class="post-meta__time">${posts[i].created}</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">${posts[i].content}</div>
+                <div class="post__image">
+                    <img src="${posts[i].media}" alt="${posts[i].author.name}-profile-image">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div>
+        `;
+    }
+    
+}   container.innerHTML += post;
+
+
+function inCharacter(word){
+    let upChar = '';
+    for (let i = 0; i < word.length; i++){
+        if (word[i] == word[i].toUpperCase()){
+            upChar += word[i]
+        }
+    }
+    return upChar;
+}
